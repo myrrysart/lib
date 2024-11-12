f7bb75157b07bbf4cdaf3a331012942f7875de0f
Jyry Niemitalo <jyniemit@c1r5p1.hive.fi>
2024-11-12 13:59:36 +0200

    calloc init

--- ft_calloc.c
+++ ft_calloc.c
@@ -21,3 +21,22 @@
 /*          !returns NULL if memory allocation fails                         */
 /*          !no protection against unreasonable allocation sizes             */
 /* ************************************************************************** */
+
+#include "libft.h"
+
+void	*ft_calloc(size_t count, size_t size)
+{
+	unsigned char	*ptr;
+	size_t			i;
+
+	ptr = NULL;
+	i = 0;
+	if (count > SIZE_MAX/size)
+		return (NULL);
+	ptr = malloc(count * size);
+	if (!ptr)
+		return (NULL);
+	while (i < count * size)
+		ptr[i++] = 0;
+	return (ptr);
+}
