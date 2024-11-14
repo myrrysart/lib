506f5361b9ff11c635d3701db84687ba110c6c29
Jyry Niemitalo <jyniemit@student.hive.fi>
2024-11-14 10:25:34 +0200

    memcpy init

--- ft_memcpy.c
+++ ft_memcpy.c
@@ -22,3 +22,22 @@
 /*          !undefined behavior if memory areas overlap                      */
 /*          !no bounds checking beyond n                                     */
 /* ************************************************************************** */
+
+#include "libft.h"
+
+void	*ft_memcpy(void *dst, const void *src, size_t n)
+{
+	size_t			i;
+	unsigned char	*res;
+	unsigned char	*sta;
+
+	res = (unsigned char *)dst;
+	sta = (unsigned char *)src;
+	i = 0;
+	while (i < n)
+	{
+		res[i] = sta[i];
+		i++;
+	}
+	return (res);
+}
