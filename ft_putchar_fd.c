5a26a36d63d9c9957957077c854e78d22c282309
Jyry Niemitalo <jyniemit@student.hive.fi>
2024-11-25 16:46:53 +0200

    init ft_putchar_fd
    write c to fd. Simple as that.

--- ft_putchar_fd.c
+++ ft_putchar_fd.c
@@ -21,3 +21,10 @@
 /*          !write errors are not handled                                    */
 /*          !does not handle wide characters                                 */
 /* ************************************************************************** */
+
+#include "libft.h"
+
+void	ft_putchar_fd(char c, int fd)
+{
+	write(fd, &c, 1);
+}
