6b56f8fef8929a57a7ee2cf753d65ae626bf3af4
Jyry Niemitalo <jyniemit@student.hive.fi>
2024-11-26 15:47:38 +0200

    fixed: Now adds \0 to the end of the string correctly

--- ft_strlcat.c
+++ ft_strlcat.c
@@ -41,6 +41,7 @@ size_t	ft_strlcat(char *dst, const char *src, size_t dstsize)
 		dst[dstlen + i] = src[i];
 		i++;
 	}
-	dst[dstlen + i] = '\0';
+	if(dstlen + i < dstsize)
+		dst[dstlen + i] = '\0';
 	return (dstlen + srclen);
 }
