587b54f48c2afb77a1d9405b05fe2744b67d9c94
Jyry Niemitalo <jyniemit@student.hive.fi>
2024-11-26 15:45:20 +0200

    fixed memcmp logic

--- ft_memcmp.c
+++ ft_memcmp.c
@@ -27,13 +27,17 @@
 int	ft_memcmp(const void *s1, const void *s2, size_t n)
 {
 	size_t			i;
-	unsigned char	*mem1;
-	unsigned char	*mem2;
+	const unsigned char	*mem1;
+	const unsigned char	*mem2;
 
 	i = 0;
-	mem1 = (unsigned char *)s1;
-	mem2 = (unsigned char *)s2;
+	mem1 = (const unsigned char *)s1;
+	mem2 = (const unsigned char *)s2;
+	if (n == 0)
+		return (0);
 	while (i < n && mem1[i] == mem2[i])
 		i++;
+	if (i == n)
+		return (0);
 	return (mem1[i] - mem2[i]);
 }
