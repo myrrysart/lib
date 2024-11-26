68705f4d896e2aba9a1282e2f0b1566ef2a2c43f
Jyry Niemitalo <jyniemit@student.hive.fi>
2024-11-26 10:51:03 +0200

    scan memory for character function init

--- ft_memchr.c
+++ ft_memchr.c
@@ -21,3 +21,21 @@
 /*   ERROR: !undefined behavior if s is NULL                                 */
 /*          !no bounds checking beyond n                                     */
 /* ************************************************************************** */
+
+#include "libft.h"
+
+void *ft_memchr(const void *s, int c, size_t n)
+{
+	size_t	i;
+	const unsigned char	*ret;
+
+	i = 0;
+	ret = s;
+	while (i < n)
+	{
+		if (ret[i] == (unsigned char)c)
+			return ((void *)(ret + i));
+		i++;
+	}
+	return (NULL);
+}
