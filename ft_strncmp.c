13b3d9ea4ca286f3afde47454c90abdcd65e7a93
Jyry Niemitalo <jyniemit@student.hive.fi>
2024-11-26 15:49:13 +0200

    fixed return to cast unsigned char and return 0 if we have reached the limit

--- ft_strncmp.c
+++ ft_strncmp.c
@@ -33,5 +33,7 @@ int ft_strncmp(const char *s1, const char *s2, size_t n)
 	while (s1[i] && s2[i] && 
 			(unsigned char)s1[i] == (unsigned char)s2[i] && n--)
 		i++;
-	return (s1[i] - s2[i]);
+	if (n)
+		return ((unsigned char)s1[i] - (unsigned char)s2[i]);
+	return (0);
 }
