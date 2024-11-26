3e2de87fb7fcd3f37917f7e50b2c9d6953f0f4b5
Jyry Niemitalo <jyniemit@student.hive.fi>
2024-11-26 10:51:14 +0200

    memory compare function init

--- ft_memcmp.c
+++ ft_memcmp.c
@@ -21,3 +21,19 @@
 /*   ERROR: !undefined behavior if s1 or s2 is NULL                         */
 /*          !no bounds checking beyond n bytes                              */
 /* ************************************************************************** */
+
+#include "libft.h"
+
+int	ft_memcmp(const void *s1, const void *s2, size_t n)
+{
+	size_t			i;
+	unsigned char	*mem1;
+	unsigned char	*mem2;
+
+	i = 0;
+	mem1 = (unsigned char *)s1;
+	mem2 = (unsigned char *)s2;
+	while (i < n && mem1[i] == mem2[i])
+		i++;
+	return (mem1[i] - mem2[i]);
+}
