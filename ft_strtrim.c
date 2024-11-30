82c5f229799c4348d08664f2df7f35d49adacc84
Jyry Niemitalo <jyniemit@student.hive.fi>
2024-11-30 15:50:05 +0200

    strtrim test version done, passes initial tests, passes empty string in all set edgecase.

--- ft_strtrim.c
+++ ft_strtrim.c
@@ -24,35 +24,67 @@
 
 #include "libft.h"
 
+static int	ft_setcheck(char const *s1, char const *set)
+{
+	int	found;
+
+	found = 0;
+	while (*set)
+	{
+		if (*set == *s1)
+		{
+			found = 1;
+			return (found);
+		}
+		set++;
+	}
+	return (found);
+}
+
 static char const	*ft_strcheckset(char const *s1, char const *set)
 {
-	char const	*set_origin;
-	int		found;
-	int		end;
-
-	found = 1;
-	set_origin = set;
-	end = 0;
-	while (found)
-	{
-		while(*set)
-		{
-			if(*set == *s1)
-			{
-				found = 1;
-				s1++;
-			}
-			else if (end)
-				found = 0;
-		}
-		end = 0;
-		set = set_origin;
-	}
+	while(ft_setcheck(s1, set))
+		s1++;
 	return (s1);
 }
+
+static char const	*ft_strchecksetrev(char const *s1,
+									 char const *s2, char const *set)
+{
+
+	while(ft_setcheck(s1, set) && s1 > s2)
+		s1--;
+	return (s1);
+}
+
 char	*ft_strtrim(char const *s1, char const *set)
 {
 	char const	*start;
+	char const	*end;
+	char		*res;
+	size_t		res_len;
 
-	start = ft_strcheckset(s1, set)
+	res = NULL;
+	res_len = 0;
+	if (!s1 || !set)
+		return (NULL);
+	start = ft_strcheckset(s1, set);
+	end = ft_strchecksetrev(&s1[ft_strlen(s1) - 1], start, set);
+	while (&start[res_len] <= end)
+		res_len++;
+	res = ft_calloc(res_len + 1, sizeof(char));
+	if (!res)
+		return (NULL);
+	ft_strlcpy(res, start, res_len + 1);
+	return (res);
+}
+
+#include <stdio.h>
+int	main(void)
+{
+	char	*test1="Testing testing testing!";
+	char	*set="Testing !";
+	
+	printf("|%s| is the string left after testing", ft_strtrim(test1, set));
+	return (0);
 }
