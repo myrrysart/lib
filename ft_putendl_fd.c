7e967160e7263241f77569ce8efc27ead6d81ea8
Jyry Niemitalo <jyniemit@student.hive.fi>
2024-12-01 17:20:34 +0200

    works, but...

--- ft_putendl_fd.c
+++ ft_putendl_fd.c
@@ -22,3 +22,11 @@
 /*          !write errors are not handled                                    */
 /*          !newline might fail even if string succeeds                      */
 /* ************************************************************************** */
+
+#include "libft.h"
+
+void ft_putendl_fd(char *s, int fd)
+{
+	write(fd, s, ft_strlen(s));
+	write(fd, "\n", 1);
+}
