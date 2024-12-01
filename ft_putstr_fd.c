c31fc59c535a625958c09ba68824285aea6761c6
Jyry Niemitalo <jyniemit@student.hive.fi>
2024-12-01 17:21:08 +0200

    putstr works

--- ft_putstr_fd.c
+++ ft_putstr_fd.c
@@ -22,3 +22,9 @@
 /*          !write errors are not handled                                    */
 /* ************************************************************************** */
 
+#include "libft.h"
+
+void	ft_putstr_fd(char *s, int fd)
+{
+	write(fd, s, ft_strlen(s));
+}
