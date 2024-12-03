c20fe47563a34c9a5e1f26fcb44d5c3bb6839380
Jyry Niemitalo <jyniemit@student.hive.fi>
2024-12-03 15:51:47 +0200

    fix(libft.h): striteri proto corrected

--- libft.h
+++ libft.h
@@ -48,7 +48,7 @@ void	ft_putstr_fd(char *s, int fd);
 char	**ft_split(char const *s, char c);
 char	*ft_strchr(const char *s, int c);
 char	*ft_strdup(const char *s1);
-int		ft_striteri(int c);
+void	ft_striteri(char *s, void (*f)(unsigned int, char*));
 char	*ft_strjoin(char const *s1, char const *s2);
 size_t	ft_strlcat(char *dst, const char *src, size_t dstsize);
 size_t	ft_strlcpy(char *dst, const char *src, size_t dstsize);
