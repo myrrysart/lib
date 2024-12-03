c227d6f4df9fb7ea774cfd164853abea9af63171
Jyry Niemitalo <jyniemit@student.hive.fi>
2024-12-03 12:37:00 +0200

    fix(libft.h): strmapi proto

--- libft.h
+++ libft.h
@@ -53,7 +53,7 @@ char	*ft_strjoin(char const *s1, char const *s2);
 size_t	ft_strlcat(char *dst, const char *src, size_t dstsize);
 size_t	ft_strlcpy(char *dst, const char *src, size_t dstsize);
 size_t	ft_strlen(const char *s);
-int		ft_strmapi(int c);
+char	*ft_strmapi(char const *s, char (*f)(unsigned int, char));
 int		ft_strncmp(const char *s1, const char *s2, size_t n);
 char	*ft_strnstr(const char *hay, const char *ndl, size_t len);
 char	*ft_strrchr(const char *s, int c);
