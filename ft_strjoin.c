16222f76d094cc2c6b7022b3067d959a1d26acb1
Jyry Niemitalo <jyniemit@student.hive.fi>
2024-12-04 12:28:15 +0200

    fix(strjoin): removed unnecessary assignment

--- ft_strjoin.c
+++ ft_strjoin.c
@@ -27,6 +27,5 @@ char	*ft_strjoin(char const *s1, char const *s2)
 		return (NULL);
 	ft_strlcpy(res, s1, len + 1);
 	ft_strlcpy(&res[ft_strlen(s1)], s2, len - ft_strlen(s1) + 1);
-	res[len] = '\0';
 	return (res);
 }
